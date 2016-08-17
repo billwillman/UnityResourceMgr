0b842d9b190eab7112d1a647f2b8f746e5b9e676
billwillman <billwillman@163.com>
2016-08-17 23:00:27 +0800

    [增加]NGUI 读取测试用例

---
+++ Assets/Scripts/TestUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUI : NGUIResLoader {
+
+	public UITexture uiTexture = null;
+	private int flag = 0;
+	void OnGUI()
+	{
+		if (m_IsConfigLoaded && uiTexture != null)
+		{
+			if (GUI.Button(new Rect(100, 100, 150, 50), "切换贴图"))
+			{
+				if (flag%2 == 0)
+					UITexture_LoadMainTexture(uiTexture, "resources/models/@flag/m_hadry_01.psd");
+				else
+					UITexture_LoadMainTexture(uiTexture, "resources/models/@flag/m_items_01.psd");
+				++flag;
+			}
+		}
+
+	}
+
+	void OnConfigLoad(bool isOK)
+	{
+		m_IsConfigLoaded = true;
+	}
+
+	void Start()
+	{
+		ResourceMgr.Instance.LoadConfigs(OnConfigLoad);
+	}
+
+	void Update()
+	{
+		TimerMgr.Instance.ScaleTick(Time.deltaTime);
+		TimerMgr.Instance.UnScaleTick(Time.unscaledDeltaTime);
+	}
+
+	private bool m_IsConfigLoaded = false;
+}
