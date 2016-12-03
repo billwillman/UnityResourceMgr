9041119a48290b6cad83511e5195dbfbcbc2dc9f
billwillman <billwillman@163.com>
2016-12-03 22:13:08 +0800

    【修改】写入文件增加锁

--- Assets/Scripts/AutoUpdate/HttpClientFileStream.cs
+++ Assets/Scripts/AutoUpdate/HttpClientFileStream.cs
@@ -18,39 +18,44 @@ namespace NsHttpClient
 		{
 			base.DoClose ();
 
-			if (m_Stream != null) {
-				m_Stream.Close ();
-				m_Stream.Dispose ();
-				m_Stream = null;
+			lock (m_StreamLock) {
+				if (m_Stream != null) {
+					m_Stream.Close ();
+					m_Stream.Dispose ();
+					m_Stream = null;
+				}
 			}
 		}
 
 		protected override void Flush (int read)
 		{
 			if (read > 0) {
-				if (m_Stream == null && !string.IsNullOrEmpty (m_WriteFileName)) {
-					FileMode mode;
-					if (m_Process <= 0)
-						mode = FileMode.Create;
-					else
-						mode = FileMode.OpenOrCreate;
-					m_Stream = new FileStream (m_WriteFileName, mode, FileAccess.Write);
-					if (m_Process > 0)
-						m_Stream.Seek (m_Process, SeekOrigin.Begin);
+				lock (m_StreamLock) {
+					if (m_Stream == null && !string.IsNullOrEmpty (m_WriteFileName)) {
+						FileMode mode;
+						if (m_Process <= 0)
+							mode = FileMode.Create;
+						else
+							mode = FileMode.OpenOrCreate;
+						m_Stream = new FileStream (m_WriteFileName, mode, FileAccess.Write);
+						if (m_Process > 0)
+							m_Stream.Seek (m_Process, SeekOrigin.Begin);
 
-					UnityEngine.Debug.Log("Http FileStream Start!!!");
+						UnityEngine.Debug.Log ("Http FileStream Start!!!");
+					}
+
+					if (m_Stream == null)
+						return;
+
+					m_Stream.Write (m_Buf, 0, read);
+					m_Stream.Flush ();
 				}
-
-				if (m_Stream == null)
-					return;
-
-				m_Stream.Write (m_Buf, 0, read);
-				m_Stream.Flush ();
 			}
 		}
 
 		private string m_WriteFileName = string.Empty;
 		private FileStream m_Stream = null;
 		private long m_Process = 0;
+		private System.Object m_StreamLock = new object ();
 	}
 }
