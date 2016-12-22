6a1c22e8c97fab3a33cacec342cc8c17c61fa067
billwillman <billwillman@qq.com>
2016-12-22 18:00:31 +0800

    【修正】Http字符串获取

--- Assets/Scripts/AutoUpdate/HttpClientStrResponse.cs
+++ Assets/Scripts/AutoUpdate/HttpClientStrResponse.cs
@@ -6,32 +6,47 @@ namespace NsHttpClient
 {
 	public class HttpClientStrResponse: HttpClientResponse
 	{
-		public HttpClientStrResponse(int bufSize = 1024): base(bufSize)
-		{}
-		
-		protected override void Flush(int read)
-		{
-			string s = System.Text.Encoding.ASCII.GetString(m_Buf, 0, read);
-			lock (m_TxtLock)
-			{
-				m_Txt += s;
-			}
-		}
-		
-		public string Txt
-		{
-			get
-			{
-				lock (m_TxtLock)
-				{
-					return m_Txt;
-				}
-					
-			}
-		}
-		
-		private string m_Txt = string.Empty;
-		private System.Object  m_TxtLock = new object();
-	}
+        public HttpClientStrResponse() : this(1024) { }
+        public HttpClientStrResponse(int bufSize) : base(bufSize) { }
+        protected override void Flush(int read) {
+            if (m_Stream == null)
+                m_Stream = new MemoryStream();
+            m_Stream.Write(m_Buf, 0, read);
+        }
+
+        protected override void End() {
+            lock (m_TxtLock) {
+                byte[] buf = m_Stream.ToArray();
+                string txt = string.Empty;
+                if (buf != null && buf.Length > 0) {
+                    txt = System.Text.Encoding.UTF8.GetString(buf);
+                }
+                lock (m_TxtLock) {
+                    m_Txt = txt;
+                }
+            }
+        }
+
+        protected override void DoClose() {
+            base.DoClose();
+            if (m_Stream != null) {
+                m_Stream.Close();
+                m_Stream.Dispose();
+                m_Stream = null;
+            }
+        }
+
+        public string Txt {
+            get {
+                lock (m_TxtLock) {
+                    return m_Txt;
+                }
+            }
+        }
+
+        private string m_Txt = string.Empty;
+        private System.Object m_TxtLock = new object();
+        private MemoryStream m_Stream = null;
+    }
 }
 
