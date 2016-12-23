0b47d0fc54997421c918586ef9e313655a6ea5f9
billwillman <billwillman@qq.com>
2016-12-23 20:04:26 +0800

    【增加】部分注释

--- Assets/Scripts/Res/IResourceLoader.cs
+++ Assets/Scripts/Res/IResourceLoader.cs
@@ -15,7 +15,7 @@ public enum ResourceCacheType
         rctNone = 0, 
         // 临时的,只用在同步的时候，立马实例化Prefab(建议不要使用)
 		rctTemp,
-        // 对原始资源进行引用计数 +1，切记把LOAD出来的资源当做指针，替换和OnDestroy的时候，不要忘记使用ResourceMgr.Instance.Destroy进行释放
+        // 对原始资源进行引用计数 +1，切记把LOAD出来的资源当做指针，替换和OnDestroy的时候，不要忘记使用ResourceMgr.Instance.Destroy对引用计数-1
 		rctRefAdd
 }
 
