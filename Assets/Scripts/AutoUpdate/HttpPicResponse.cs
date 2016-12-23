4794af730ff45644636f903fea0a58ddb0bc9e62
billwillman <billwillman@163.com>
2016-12-23 08:50:13 +0800

    【增加】通过HTTP下载图片Response

---
+++ Assets/Scripts/AutoUpdate/HttpPicResponse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace NsHttpClient
+{
+	public class HttpClientPicResponse: HttpClientResponse  {
+
+		// 主线程调用
+		public HttpClientPicResponse(int width, int height, int bufSize = 1024): base(bufSize)
+		{
+			m_Width = width;
+			m_Height = height;
+		}
+
+		// 子线程调用的
+		protected override void Flush(int read)
+		{
+			if (m_Stream == null)
+				m_Stream = new MemoryStream();
+			m_Stream.Write(m_Buf, 0, read);
+		}
+
+		// 子线程调用的
+		protected override void DoClose()
+		{
+			base.DoClose();
+			byte[] picBuf = null;
+			if (m_Stream != null)
+			{
+				picBuf = m_Stream.ToArray();
+				m_Stream.Close();
+				m_Stream.Dispose();
+				m_Stream = null;
+			}
+
+			lock (m_Lock)
+			{
+				m_PicBuf = picBuf;
+			}
+		}
+
+		// 主线程调用
+		public Texture2D GeneratorTexture()
+		{
+			lock (m_Lock)
+			{
+				if (m_Width <= 0 || m_Height <= 0 || m_PicBuf == null || m_PicBuf.Length <= 0)
+					return null;
+
+				Texture2D ret = new Texture2D(m_Width, m_Height);
+				ret.LoadImage(m_PicBuf);
+				return ret;
+			}
+		}
+
+		private int m_Width = 0;
+		private int m_Height = 0;
+		private MemoryStream m_Stream;
+		private byte[] m_PicBuf = null;
+		private System.Object m_Lock = new object();
+	}
+}
