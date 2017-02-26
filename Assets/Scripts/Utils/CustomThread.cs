8b359cb037550a5849aafaf601dfa8639c471b69
billwillman <billwillman@163.com>
2017-02-26 20:44:57 +0800

    【增加】自己的线程封装框架 【增加】解压线程封装

---
+++ Assets/Scripts/Utils/CustomThread.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace Utils
+{
+	public class CustomThread: DisposeObject
+	{
+		// 线程是否在运行
+        public bool IsThreadRuning
+        {
+            get {
+                return m_Thread != null && m_Thread.ThreadState == ThreadState.Running 
+                    && LocalThreadState == ThreadState.Running;
+            }
+        }
+
+        public CustomThread()
+        {
+            LocalThreadState = ThreadState.Unstarted;
+            m_IsCallEnd = false;
+        }
+
+        protected virtual void End()
+        {}
+
+        private void CallEnd()
+        {
+            if (m_IsCallEnd)
+                return;
+            m_IsCallEnd = true;
+
+            End ();
+        }
+
+        private void OnCheckEndStatus(Timer obj, float timer)
+        {
+            if (LocalThreadState == ThreadState.Aborted) {
+                CallEnd ();
+                if (m_Time != null) {
+                    m_Time.Dispose ();
+                    m_Time = null;
+                }
+            }
+        }
+
+        // 开始线程
+        public void Start()
+        {
+            if (m_Time == null) {
+                m_Time = TimerMgr.Instance.CreateTimer (false, 0, true, true);
+                m_Time.AddListener (OnCheckEndStatus);
+            }
+
+            if (m_Thread == null) {
+                m_IsCallEnd = false;
+                m_Thread = new Thread (ThreadProc);
+                LocalThreadState = ThreadState.Running;
+                m_Thread.Start ();
+            }
+        }
+
+        protected override void OnFree(bool isManual)
+        {
+            // isCallEnd目的：保证OnEnd事件里可以再调用Dispose不会陷入死循环
+            bool isCallEnd = false;
+            if (m_Thread != null) {
+                Abort ();
+                m_Thread.Join();
+                isCallEnd = true;
+            }  
+
+            if (m_Time != null) {
+                m_Time.Dispose ();
+                m_Time = null;
+            }
+                
+            m_Thread = null;
+
+            if (isCallEnd)
+                CallEnd ();
+        }
+
+        protected virtual void Execute()
+        {}
+
+        private void ThreadProc()
+        {
+            while (IsThreadRuning) {
+                try
+                {
+                    Execute();
+                    Thread.Sleep(1);
+                } catch (ThreadAbortException ex) {
+                    #if DEBUG
+                    // 不做处理
+					UnityEngine.Debug.Log(ex.ToString());
+                    #endif
+                }
+            }
+
+            LocalThreadState = ThreadState.Aborted;
+        }
+
+        protected void Abort()
+        {
+            if (LocalThreadState == ThreadState.Running)
+                LocalThreadState = ThreadState.AbortRequested;
+        }
+
+        // 用于模拟abort
+        protected ThreadState LocalThreadState {
+            get {
+                lock (m_Mutex) {
+                    return m_ThreadStatus;
+                }
+            }
+
+            set {
+                lock (m_Mutex) {
+                    m_ThreadStatus = value;
+                }
+            }
+        }
+
+        // 线程句柄
+        private Thread m_Thread = null;
+        // 线程状态
+        private ThreadState m_ThreadStatus = ThreadState.Unstarted;
+        // 锁
+        private System.Object m_Mutex = new object();
+        // 检测线程状态
+        private Timer m_Time = null;
+
+        private bool m_IsCallEnd = false;
+
+	}
+}
---
+++ Assets/Scripts/Utils/UnZipThread.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace Utils
+{
+	public class UnZipThread: CustomThread
+	{
+		public enum UnZipStatus
+        {
+            // 无状态
+            uzNone,
+            // 正在解压
+            uzDoing,
+            // 解压完成
+            uzOk,
+            // 解压失败
+            uzFail
+        }
+
+        public UnZipThread(string fileName, string exportDir = "", bool isUnZipDelete = true, Action<bool> onEnd = null): base()
+        {
+            m_UnZipDelete = isUnZipDelete;
+            m_FileName = fileName;
+            m_ExportDir = exportDir;
+            OnZipedEnd = onEnd;
+            if (!string.IsNullOrEmpty (m_ExportDir)) {
+                if (!m_ExportDir.EndsWith("/"))
+                    m_ExportDir += "/";
+            }
+            if (!File.Exists (m_FileName)) {
+                Status = UnZipStatus.uzFail;
+                if (OnZipedEnd != null) {
+                    OnZipedEnd (false);
+                }
+                return;
+            }
+
+            Process = 0f;
+            m_Cnt = 0;
+            m_Cur = 0;
+
+            Start ();
+        }
+
+        // 解压进度
+        public float Process
+        {
+            get
+            {
+                lock (this) {
+                    return m_Process;
+                }
+            }
+
+            private set {
+                lock (this) {
+                    m_Process = value;
+                }
+            }
+        }
+
+        private Action<bool> OnZipedEnd
+        {
+            get;
+            set;
+        }
+
+        // 解压状态
+        public UnZipStatus Status
+        {
+            get
+            {
+                lock (this) {
+                    return m_UnZipStatus;
+                }
+            }
+
+            private set {
+                lock (this) {
+                    if (m_UnZipStatus == value)
+                        return;
+                    m_UnZipStatus = value;
+                }
+            }
+        }
+
+        // 初始化UNZIP
+        private void InitUnZip()
+        {
+            if (m_Stream != null)
+                return;
+            m_Stream = new ZipInputStream (File.OpenRead (m_FileName));
+            m_Cnt = m_Stream.Length;
+            Process = 0;
+        }
+		
+		private byte[] m_UnZipBuf = new byte[2048];
+
+        private void UnzipOneFile(ZipEntry theEntry, ZipInputStream s, string strDirectory, bool overWrite) {
+            string directoryName = string.Empty;
+            string pathToZip = theEntry.Name;
+            if (!string.IsNullOrEmpty(pathToZip))
+                directoryName = Path.GetDirectoryName(pathToZip) + "/";
+
+            string fileName = Path.GetFileName(pathToZip);
+
+            string exportDir = strDirectory + directoryName;
+            if (!Directory.Exists(exportDir))
+                Directory.CreateDirectory(exportDir);
+
+            if (!string.IsNullOrEmpty(fileName)) {
+                string newFileName = exportDir + fileName;
+                if ((File.Exists(newFileName) && overWrite)
+                    || (!File.Exists(newFileName))) {
+                    using (FileStream streamWriter = new FileStream(newFileName, FileMode.Create, FileAccess.Write)) {
+                        float d = (float)theEntry.CompressedSize / (float)theEntry.Size;
+                        int size = m_UnZipBuf.Length;
+                        byte[] data = m_UnZipBuf;
+                        while (true) {
+                            size = s.Read(data, 0, data.Length);
+
+                            if (size > 0) {
+                                m_Cur += size * d;
+                                Process = ((float)m_Cur) / ((float)m_Cnt);
+                        //       UnityEngine.Debug.LogFormat ("UnZip Process: {0}", Process.ToString ());
+                                streamWriter.Write(data, 0, size);
+                                streamWriter.Flush ();
+
+                        //        System.Threading.Thread.Sleep (1);
+                            }
+                            else {
+                              //  m_Cur += theEntry.CompressedSize;
+                              //  Process = ((float)m_Cur) / ((float)m_Cnt);
+                            //    UnityEngine.Debug.LogFormat ("UnZip Process: {0}", Process.ToString ());
+                                break;
+                            }
+                        }
+                        streamWriter.Close();
+                        streamWriter.Dispose ();
+                    }
+                }
+            }
+
+        }
+
+        private void ProcessUnZip()
+        {
+            if (m_Stream != null) {
+                Status = UnZipStatus.uzDoing;
+                ZipEntry theEntry;
+                while ((theEntry = m_Stream.GetNextEntry ()) != null) {
+              //      Logger.StartTime ();
+                    UnzipOneFile(theEntry, m_Stream, m_ExportDir, true);
+                //    Logger.StopTime (theEntry.Name);
+
+          //          System.Threading.Thread.Sleep (1);
+                }
+                    
+                Status = UnZipStatus.uzOk;
+                // 退出线程
+                Abort ();
+            }
+        }
+
+        // 子线程调用
+        protected override void Execute()
+        {
+            try
+            {
+                InitUnZip();
+                ProcessUnZip();
+            } catch
+            {
+                // 失败
+                Status = UnZipStatus.uzFail;
+                Abort ();
+            }
+        }
+
+
+        protected override void End()
+        {
+            ClearStream ();
+
+            if (m_UnZipDelete) {
+                if (File.Exists (m_FileName)) {
+                    File.Delete (m_FileName);
+                }
+            }
+
+            if (OnZipedEnd != null) {
+                OnZipedEnd (Status == UnZipStatus.uzOk);
+            }
+
+        }
+
+        private void ClearStream()
+        {
+            if (m_Stream != null) {
+                m_Stream.Close ();
+                m_Stream.Dispose ();
+                m_Stream = null;
+            }
+        }
+
+
+        private string m_FileName = string.Empty;
+        private string m_ExportDir = string.Empty;
+        private bool m_UnZipDelete = false;
+        private ZipInputStream m_Stream = null;
+        private float m_Process = 0;
+        private float m_Cnt = 0;
+        private float m_Cur = 0;
+        private UnZipStatus m_UnZipStatus = UnZipStatus.uzNone;
+
+
+	}
+}
