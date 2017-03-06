e381feac3d2273cb4ad8e4b61cd7b6da93298c3f
billwillman <billwillman@163.com>
2017-03-06 22:58:37 +0800

    【修改】VERSION.TXT和FILELIST.TXT采用新的HTTP框架

--- Assets/Scripts/AutoUpdate/AutoUpdateCheckVersionState.cs
+++ Assets/Scripts/AutoUpdate/AutoUpdateCheckVersionState.cs
@@ -18,6 +18,32 @@ namespace AutoUpdate
 			AutoUpdateMgr.Instance.ChangeState(AutoUpdateState.auGetZipVerReq);
 		}
 
+		void DoVersionTxt(string versionStr)
+		{
+			if (!string.IsNullOrEmpty(versionStr))
+			{
+				AutoUpdateMgr.Instance.LoadServerResVer(versionStr);
+				string resVer = AutoUpdateMgr.Instance.CurrServeResrVersion;
+				if (!string.IsNullOrEmpty(resVer))
+				{
+					if (AutoUpdateMgr.Instance.IsVersionNoUpdate())
+					{
+						// CheckFileList
+
+						AutoUpdateMgr.Instance.EndAutoUpdate();
+					}
+					#if USE_ZIPVER
+					else if (!AutoUpdateMgr.Instance.IsVersionTxtNoUpdate())
+					ToGetZipVerReq();
+					#endif
+					else
+						ToResListState();	
+				} else
+					AutoUpdateMgr.Instance.EndAutoUpdate();
+			} else
+				AutoUpdateMgr.Instance.EndAutoUpdate();
+		}
+
 		void OnReadEvent(HttpClientResponse response, long totalReadBytes)
 		{
 			if (totalReadBytes >= response.MaxReadBytes)
@@ -25,34 +51,30 @@ namespace AutoUpdate
 				// 判断版本
 				HttpClientStrResponse r = response as HttpClientStrResponse;
 				string versionStr = r.Txt;
-				if (!string.IsNullOrEmpty(versionStr))
-				{
-					AutoUpdateMgr.Instance.LoadServerResVer(versionStr);
-					string resVer = AutoUpdateMgr.Instance.CurrServeResrVersion;
-					if (!string.IsNullOrEmpty(resVer))
-					{
-						if (AutoUpdateMgr.Instance.IsVersionNoUpdate())
-						{
-							// CheckFileList
-
-							AutoUpdateMgr.Instance.EndAutoUpdate();
-						}
-						#if USE_ZIPVER
-						else if (!AutoUpdateMgr.Instance.IsVersionTxtNoUpdate())
-							ToGetZipVerReq();
-						#endif
-						else
-							ToResListState();	
-					} else
-						AutoUpdateMgr.Instance.EndAutoUpdate();
-				} else
-					AutoUpdateMgr.Instance.EndAutoUpdate();
+				DoVersionTxt(versionStr);
 			}
 		}
 		
 		void OnError(HttpClientResponse response, int status)
 		{
 			AutoUpdateMgr.Instance.Error(AutoUpdateErrorType.auError_NoGetVersion, status);
+		}
+
+		void OnEnd(HttpClient client, HttpListenerStatus status)
+		{
+			switch (status)
+			{
+			case HttpListenerStatus.hsDone:
+				{
+					HttpClientStrResponse r = client.Listener as HttpClientStrResponse;
+					string versionStr = r.Txt;
+					DoVersionTxt(versionStr);
+					break;
+				}
+			case HttpListenerStatus.hsError:
+				AutoUpdateMgr.Instance.Error(AutoUpdateErrorType.auError_NoGetVersion, -1);
+				break;
+			}
 		}
 		
 		public override  void Enter(AutoUpdateMgr target)
@@ -68,8 +90,9 @@ namespace AutoUpdate
 				url = string.Format("{0}/{1}?time={2}", resAddr, AutoUpdateMgr._cVersionTxt, t.ToString());
 			}
 
+			target.CreateHttpTxt(url, OnEnd);
+		   // target.CreateHttpTxt(url, OnReadEvent, OnError);
 
-		    target.CreateHttpTxt(url, OnReadEvent, OnError);
 		}
 
 	}
--- Assets/Scripts/AutoUpdate/AutoUpdateGetFileListState.cs
+++ Assets/Scripts/AutoUpdate/AutoUpdateGetFileListState.cs
@@ -12,50 +12,72 @@ namespace AutoUpdate
 			AutoUpdateMgr.Instance.ChangeState(AutoUpdateState.auUpdateFileProcess);
 		}
 
+		void DoFileListTxt(string fileList)
+		{
+			if (string.IsNullOrEmpty(fileList))
+			{
+				AutoUpdateMgr.Instance.EndAutoUpdate();
+				return;
+			}
+
+			string writePath = AutoUpdateMgr.Instance.WritePath;
+			string srcFileName = string.Format("{0}/{1}", writePath, AutoUpdateMgr._cFileListTxt);
+			if (!File.Exists(srcFileName))
+			{
+				// 直接生成写到update.txt里
+				ResListFile resListFile = AutoUpdateMgr.Instance.ServerResListFile;
+				resListFile.Load(fileList);
+				ResListFile.ResDiffInfo[] infos = resListFile.AllToDiffInfos();
+
+				AutoUpdateMgr.Instance.UpdateToUpdateTxt(infos);
+				AutoUpdateMgr.Instance.UpdateTotalDownloadBytes(infos);
+
+				ToNextState();
+				return;
+			}
+
+			ResListFile srcListFile = AutoUpdateMgr.Instance.LocalResListFile;
+			ResListFile dstListFile = AutoUpdateMgr.Instance.ServerResListFile;
+
+			dstListFile.Load(fileList);
+
+			ResListFile.ResDiffInfo[] diffInfos = srcListFile.GetDiffInfos(dstListFile);
+
+			AutoUpdateMgr.Instance.UpdateToUpdateTxt(diffInfos);
+			AutoUpdateMgr.Instance.UpdateTotalDownloadBytes(diffInfos);
+			ToNextState();
+		}
+
 		void OnReadEvent(HttpClientResponse response, long totalReadBytes)
 		{
 			if (totalReadBytes >= response.MaxReadBytes)
 			{
 				HttpClientStrResponse r = response as HttpClientStrResponse;
 				string fileList = r.Txt;
-				if (string.IsNullOrEmpty(fileList))
-				{
-					AutoUpdateMgr.Instance.EndAutoUpdate();
-					return;
-				}
-				
-				string writePath = AutoUpdateMgr.Instance.WritePath;
-				string srcFileName = string.Format("{0}/{1}", writePath, AutoUpdateMgr._cFileListTxt);
-				if (!File.Exists(srcFileName))
-				{
-					// 直接生成写到update.txt里
-					ResListFile resListFile = AutoUpdateMgr.Instance.ServerResListFile;
-					resListFile.Load(fileList);
-					ResListFile.ResDiffInfo[] infos = resListFile.AllToDiffInfos();
-
-					AutoUpdateMgr.Instance.UpdateToUpdateTxt(infos);
-					AutoUpdateMgr.Instance.UpdateTotalDownloadBytes(infos);
-
-					ToNextState();
-					return;
-				}
-	
-				ResListFile srcListFile = AutoUpdateMgr.Instance.LocalResListFile;
-				ResListFile dstListFile = AutoUpdateMgr.Instance.ServerResListFile;
-
-				dstListFile.Load(fileList);
-					
-				ResListFile.ResDiffInfo[] diffInfos = srcListFile.GetDiffInfos(dstListFile);
-
-				AutoUpdateMgr.Instance.UpdateToUpdateTxt(diffInfos);
-				AutoUpdateMgr.Instance.UpdateTotalDownloadBytes(diffInfos);
-				ToNextState();
+				DoFileListTxt(fileList);
 			}
 		}
 		
 		void OnError(HttpClientResponse response, int status)
 		{
 			AutoUpdateMgr.Instance.Error(AutoUpdateErrorType.auError_NoGetFileList, status);
+		}
+
+		void OnEnd(HttpClient client, HttpListenerStatus status)
+		{
+			switch (status)
+			{
+			case HttpListenerStatus.hsDone:
+				{
+					HttpClientStrResponse r = client.Listener as HttpClientStrResponse;
+					string fileList = r.Txt;
+					DoFileListTxt(fileList);
+					break;
+				}
+			case HttpListenerStatus.hsError:
+				AutoUpdateMgr.Instance.Error(AutoUpdateErrorType.auError_NoGetFileList, -1);
+				break;
+			}
 		}
 		
 		void DoGetServerFileList()
@@ -65,7 +87,8 @@ namespace AutoUpdate
 			// use fileList ContentMD5
 			string serverFileListMd5 = AutoUpdateMgr.Instance.ServerFileListContentMd5;
 			string url = string.Format("{0}/{1}/{2}.txt", resAddr, ver, serverFileListMd5);
-			AutoUpdateMgr.Instance.CreateHttpTxt(url, OnReadEvent, OnError);
+			//AutoUpdateMgr.Instance.CreateHttpTxt(url, OnReadEvent, OnError);
+			AutoUpdateMgr.Instance.CreateHttpTxt(url, OnEnd); 
 		}
 		
 		public override  void Enter(AutoUpdateMgr target)
