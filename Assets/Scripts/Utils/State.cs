9d61fd18d07d9cac6c8a99cb5f0e5bfe466272bc
billwillman <billwillman@qq.com>
2017-06-06 12:17:44 +0800

    [修正]STATE状态有GC

--- Assets/Scripts/Utils/State.cs
+++ Assets/Scripts/Utils/State.cs
@@ -3,101 +3,87 @@ using System.Collections.Generic;
 
 namespace Utils
 {
-	public interface IState<U, V>
-	{
-		bool CanEnter(V target);
-		bool CanExit(V target);
-		void Enter(V target);
-		void Exit(V target);
-		void Process(V target);
+    public interface IState<U, V> {
+        bool CanEnter(V target);
+        bool CanExit(V target);
+        void Enter(V target);
+        void Exit(V target);
+        void Process(V target);
 
-		U Id
-		{
-			get;
-			set;
-		}
-	}
+        U Id {
+            get;
+            set;
+        }
+    }
 
-	public class StateMgr<U, V> where V: class
-	{
-		public StateMgr(V target)
-		{
-			mTarget = target;
-		}
+    public class StateMgr<U, V> where V : class {
+        public StateMgr(V target) {
+            m_Target = target;
+        }
 
-		public virtual void Process(V target)
-		{
-			IState<U, V> state = CurrState;
-			if (state != null)
-				state.Process(target);
-		}
+        public virtual void Process(V target) {
+            IState<U, V> state = CurrState;
+            if (state != null)
+                state.Process(target);
+        }
 
-		public virtual bool ChangeState(U id)
-		{
-			if (mTarget == null)
-				return false;
+        public virtual bool ChangeState(U id) {
+            if (m_Target == null)
+                return false;
 
-			IState<U, V> now;
-			if (mStateMap.TryGetValue(mCurrState, out now))
-			{
-				if (!now.CanExit(mTarget))
-					return false;
-			}
+            IState<U, V> now = m_CurrStatus;
+            if (now != null) {
+                if (!now.CanExit(m_Target))
+                    return false;
+            }
 
-			IState<U, V> news;
-			if (mStateMap.TryGetValue(id, out news) && news != null)
-			{
-				if (!news.CanEnter(mTarget))
-					return false;
-			} else
-				return false;
+            IState<U, V> news;
+            if (m_StateMap.TryGetValue(id, out news) && news != null) {
+                if (!news.CanEnter(m_Target))
+                    return false;
+            } else
+                return false;
 
-			if (now != null)
-				now.Exit(mTarget);
-			news.Enter(mTarget);
-			mCurrState = id;
-			return true;
-		}
+            if (now != null)
+                now.Exit(m_Target);
+            news.Enter(m_Target);
 
-		public U CurrStateKey
-		{
-			get
-			{
-				return mCurrState;
-			}
-		}
+            m_CurrKey = id;
+            m_CurrStatus = news;
+            return true;
+        }
 
-		public IState<U, V> CurrState
-		{
-			get
-			{
-				IState<U, V> ret;
-				if (!mStateMap.TryGetValue(mCurrState, out ret))
-					ret = null;
-				return ret;
-			}
-		}
+        public U CurrStateKey {
+            get {
+                return m_CurrKey;
+            }
+        }
 
-	public static void Register(U id, IState<U, V> state)
-	{
-		IState<U, V> now;
-		if (mStateMap.TryGetValue(id, out now))
-			mStateMap[id] = state;
-		else
-		{
-			mStateMap.Add(id, state);
-		}
-	}
+        public IState<U, V> CurrState {
+            get {
+                return m_CurrStatus;
+            }
+        }
 
-	public static bool FindState(U id, out IState<U, V> target)
-	{
-		bool ret = mStateMap.TryGetValue(id, out target);
-		return ret;
-	}
+        public static void Register(U id, IState<U, V> state) {
+            IState<U, V> now;
+            if (m_StateMap.TryGetValue(id, out now))
+                m_StateMap[id] = state;
+            else {
+                m_StateMap.Add(id, state);
+            }
+        }
 
-	protected V mTarget;
-	protected static Dictionary<U, IState<U, V>> mStateMap = new Dictionary<U, IState<U, V>>();
-	protected U mCurrState;
+        public static bool FindState(U id, out IState<U, V> target) {
+            bool ret = m_StateMap.TryGetValue(id, out target);
+            return ret;
+        }
+
+        protected V m_Target;
+        protected static Dictionary<U, IState<U, V>> m_StateMap = new Dictionary<U, IState<U, V>>();
+        protected U m_CurrKey;
+        protected IState<U, V> m_CurrStatus = null;
+    }
 }
 
 }
