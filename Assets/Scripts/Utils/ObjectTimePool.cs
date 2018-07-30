ca9edd8fcb2870750049c766779d53f517429137
billwillman <billwillman@qq.com>
2018-07-30 17:26:35 +0800

    [修复]BUGLY的报错

--- Assets/Scripts/Utils/ObjectTimePool.cs
+++ Assets/Scripts/Utils/ObjectTimePool.cs
@@ -211,13 +211,16 @@ namespace Utils
 			{
 				obj.UsedTime = GetCurrentTime();
 				obj.State = ITimePoolObj<K>.TimeObjState.to_NotUsed;
-				// ------保护一次
-                if (obj.LinkNode.List != null) {
-                    obj.LinkNode.List.Remove(obj.LinkNode);
-                }
-                //----------------
-				m_NotUsedList.AddLast(obj.LinkNode);
-				m_NotUsedHashMap.Add(obj.GetHashKey(), obj);
+				if (obj.LinkNode != null)
+				{
+					// ------保护一次
+					if (obj.LinkNode.List != null) {
+						obj.LinkNode.List.Remove(obj.LinkNode);
+					}
+					//----------------
+					m_NotUsedList.AddLast(obj.LinkNode);
+					m_NotUsedHashMap.Add(obj.GetHashKey(), obj);
+				}
 			}
 		}
 
