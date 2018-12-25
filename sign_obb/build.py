2ba880241e847767713c4a2d8ac48e7e9c4a1bc1
billwillman <billwillman@qq.com>
2018-12-25 14:54:36 +0800

    【修正】MD5

--- sign_obb/build.py
+++ sign_obb/build.py
@@ -249,12 +249,19 @@ def getApkInfoFromLog(logFile):
 def MonitorLine(txt):
     print txt
 
-def md5sum(filename):
-    fd = open(filename,"r")
-    fcont = fd.read()
-    fd.close()
-    fmd5 = hashlib.md5(fcont)
-    return fmd5.hexdigest();
+#较大文件处理
+def md5sum(file_path):
+  f = open(file_path,'rb')
+  md5_obj = hashlib.md5()
+  while True:
+    d = f.read(8096)
+    if not d:
+      break
+    md5_obj.update(d)
+  hash_code = md5_obj.hexdigest()
+  f.close()
+  md5 = str(hash_code).lower()
+  return md5
 
 #修改settings.xml的设置
 def writeObbSettings(settingfileName, obbFileName):
@@ -275,9 +282,9 @@ def writeObbSettings(settingfileName, obbFileName):
     newNode = ET.SubElement(root, "bool", {"name": fmd5});
     newNode.text = "True";
 
-    print "obb md5=> %s" % fmd5;
+    print "obb %s md5=> %s" % (obbFileName, fmd5);
 
-    tree.write(settingfileName);
+    tree.write(settingfileName, encoding="utf-8");
     print "写入Settings.xml完毕..."
     return;
 
