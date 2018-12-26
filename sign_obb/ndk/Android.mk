97fb6bf312414309f527e88e645f888028f963e7
billwillman <billwillman@qq.com>
2018-12-26 19:51:50 +0800

    【修改】Android.mk

--- sign_obb/ndk/Android.mk
+++ sign_obb/ndk/Android.mk
@@ -3,7 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 #LOCAL_MODULE表示生成的动态库名为NsEncry
 LOCAL_MODULE    := NsEncry
-LOCAL_C_INCLUDE := libEncry.h \
+LOCAL_MODULE_FILENAME := libNsEncry
+LOCAL_C_INCLUDE := $(LOCAL_PATH)
 #LOCAL_SRC_FILES表示使用到的类
 #LOCAL_SRC_FILES := libEncry.cpp \
 
