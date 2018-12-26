acaca150225ede44b1968cb5802e29db54e644c0
billwillman <billwillman@qq.com>
2018-12-26 19:48:10 +0800

    【修正】

--- sign_obb/ndk/libEncry.cpp
+++ sign_obb/ndk/libEncry.cpp
@@ -7,7 +7,7 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN , "Unity", __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR , "Unity", __VA_ARGS__)
 
-JNIEXPORT void JNICALL Java_com_package_NsEncry_NsEncry_CheckSign(JNIEnv* env)
+JNIEXPORT void JNICALL Java_com_NsEncryPackage_NsEncry_NsEncry_CheckSign(JNIEnv* env)
 {
 	LOGD("abc");
 }
