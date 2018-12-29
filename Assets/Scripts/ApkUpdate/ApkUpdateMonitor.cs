d351dc2d2169e2b381b71159e19d64be9aef2285
billwillman <billwillman@163.com>
2018-12-30 00:08:40 +0800

    【增加】

--- Assets/Scripts/ApkUpdate/ApkUpdateMonitor.cs
+++ Assets/Scripts/ApkUpdate/ApkUpdateMonitor.cs
@@ -6,7 +6,32 @@ namespace NsLib.ApkUpdate
     // Apk更新监控器
     public class ApkUpdateMonitor : MonoBehaviour
     {
+        private static ApkUpdateMonitor m_Instance = null;
+
         private ApkUpdateStateMgr m_Mgr = null;
+
+        private string m_Https_CurApkVer = string.Empty;
+        private string m_Https_ApkDiffs = string.Empty;
+
+        public static ApkUpdateMonitor GetInstance()
+        {
+            if (m_Instance == null)
+            {
+                GameObject obj = new GameObject("ApkUpdateMonitor", typeof(ApkUpdateMonitor));
+                m_Instance = obj.GetComponent<ApkUpdateMonitor>();
+            }
+            return m_Instance;
+        }
+
+        public static ApkUpdateMonitor Instance
+        {
+            get
+            {
+                return GetInstance();
+            }
+        }
+
+        
 
         void Awake()
         {
