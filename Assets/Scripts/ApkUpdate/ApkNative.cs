e8d58cc7e2128524b336edbfeb741de02c0d9acd
billwillman <billwillman@qq.com>
2019-01-02 12:34:11 +0800

    【增加】ApkNative.cs

---
+++ Assets/Scripts/ApkUpdate/ApkNative.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace NsLib.ApkUpdate
+{
+    internal static class ApkNative
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private static AndroidJavaClass m_fee = new AndroidJavaClass("com.UnityResources.Test.MainActivity");
+#endif
+        // 返回负数说明有错误
+        static public int GetCurrentVersionCode()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            string verCode = m_fee.CallStatic<string>("GetCurrentVersionCode");
+            Debug.LogFormat("current VersionCode: %s", verCode);
+            int ret;
+            if (!int.TryParse(verCode, out ret))
+                ret = -1;
+            return ret;
+#else
+            return 1;
+#endif
+        }
+    }
+}
