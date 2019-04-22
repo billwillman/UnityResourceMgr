0bda8f26a549a34a33c27d46db67555016a06502
billwillman <billwillman@qq.com>
2019-04-22 12:16:57 +0800

    【增加】ARRAYSIZE和MESHUTIL

---
+++ Assets/Scripts/Utils/ArraySize.cs
@@ -0,0 +1,103 @@
+﻿#if UNITY_2018_1_OR_NEWER
+#define _USE_NATIVE_ARRAY
+#endif
+
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+#if _USE_NATIVE_ARRAY
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+#endif
+
+public static class UnsafeUtil {
+    public static float UintToFloat(uint u) {
+        unsafe {
+            return *(float*)&u;
+        }
+    }
+
+    public static uint FloatToUint(float f) {
+        unsafe {
+            return *(uint*)&f;
+        }
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    private struct ArrayHeader {
+        internal IntPtr type;
+        internal int length;
+    }
+
+    private unsafe static void HackArraySizeCall<TA>(TA[] array, ArrayHeader* header, int size, Action<TA[]> func) {
+        int oriLen = header->length;
+        header->length = size;
+        try {
+            func(array);
+        } finally {
+            header->length = oriLen;
+        }
+    }
+
+    public unsafe static void IntegerHackArraySizeCall(int[] array, int size, Action<int[]> func) {
+        if (array != null && size < array.Length) {
+            fixed (void* p = array) {
+                HackArraySizeCall(array, ((ArrayHeader*)p) - 1, size, func);
+                return;
+            }
+        }
+        func(array);
+    }
+    public unsafe static void Vector2HackArraySizeCall(Vector2[] array, int size, Action<Vector2[]> func) {
+        if (array != null && size < array.Length) {
+            fixed (void* p = array) {
+                HackArraySizeCall(array, ((ArrayHeader*)p) - 1, size, func);
+                return;
+            }
+        }
+        func(array);
+    }
+    public unsafe static void Vector3HackArraySizeCall(Vector3[] array, int size, Action<Vector3[]> func) {
+        if (array != null && size < array.Length) {
+            fixed (void* p = array) {
+                HackArraySizeCall(array, ((ArrayHeader*)p) - 1, size, func);
+                return;
+            }
+        }
+        func(array);
+    }
+    public unsafe static void Vector4HackArraySizeCall(Vector4[] array, int size, Action<Vector4[]> func) {
+        if (array != null && size < array.Length) {
+            fixed (void* p = array) {
+                HackArraySizeCall(array, ((ArrayHeader*)p) - 1, size, func);
+                return;
+            }
+        }
+        func(array);
+    }
+    public unsafe static void Color32HackArraySizeCall(Color32[] array, int size, Action<Color32[]> func) {
+        if (array != null && size < array.Length) {
+            fixed (void* p = array) {
+                HackArraySizeCall(array, ((ArrayHeader*)p) - 1, size, func);
+                return;
+            }
+        }
+        func(array);
+    }
+    public unsafe static void BoneWeightHackArraySizeCall(BoneWeight[] array, int size, Action<BoneWeight[]> func) {
+        if (array != null && size < array.Length) {
+            fixed (void* p = array) {
+                HackArraySizeCall(array, ((ArrayHeader*)p) - 1, size, func);
+                return;
+            }
+        }
+        func(array);
+    }
+
+#if _USE_NATIVE_ARRAY
+    public unsafe static T UncheckReadArrayElement<T>(NativeArray<T> narr, int idx) where T : struct {
+        void* ptr = NativeArrayUnsafeUtility.GetUnsafeBufferPointerWithoutChecks<T>(narr);
+        return UnsafeUtility.ReadArrayElement<T>(ptr, idx);
+    }
+#endif
+}
---
+++ Assets/Scripts/Utils/MeshUtil.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshUtil {
+
+    public static Vector3[] GetWorldVertices(GameObject gameObject) {
+        Vector3[] aVertices = null;
+        SkinnedMeshRenderer skin;
+        MeshFilter meshFilter;
+        Mesh sharedMesh;
+
+        if ((skin = gameObject.GetComponent<SkinnedMeshRenderer>()) != null) {
+            if ((sharedMesh = skin.sharedMesh) == null) {
+                return null;
+            }
+
+            aVertices = sharedMesh.vertices;
+
+            BoneWeight[] aBoneWeights = sharedMesh.boneWeights;
+            Matrix4x4[] aBindPoses = sharedMesh.bindposes;
+            Transform[] aBones = skin.bones;
+
+            if (aVertices == null || aBoneWeights == null || aBindPoses == null || aBones == null || aBoneWeights.Length == 0 || aBindPoses.Length == 0 || aBones.Length == 0) {
+                return null;
+            }
+
+            for (int nVertex = 0; nVertex < aVertices.Length; nVertex++) {
+                BoneWeight bw = aBoneWeights[nVertex];
+                Vector4 v = aVertices[nVertex];
+                v.w = 1;
+                Vector3 v3World = aBones[bw.boneIndex0].localToWorldMatrix * aBindPoses[bw.boneIndex0] * v * bw.weight0
+                + aBones[bw.boneIndex1].localToWorldMatrix * aBindPoses[bw.boneIndex1] * v * bw.weight1
+                + aBones[bw.boneIndex2].localToWorldMatrix * aBindPoses[bw.boneIndex2] * v * bw.weight2
+                + aBones[bw.boneIndex3].localToWorldMatrix * aBindPoses[bw.boneIndex3] * v * bw.weight3;
+
+                aVertices[nVertex] = v3World;
+            }
+        } else if ((meshFilter = gameObject.GetComponent<MeshFilter>()) != null) {
+            if ((sharedMesh = meshFilter.sharedMesh) == null) {
+                return null;
+            }
+
+            aVertices = sharedMesh.vertices;
+
+            if (aVertices == null) {
+                return null;
+            }
+
+            for (int nVertex = 0; nVertex < aVertices.Length; nVertex++) {
+                aVertices[nVertex] = gameObject.transform.TransformPoint(aVertices[nVertex]);
+            }
+        }
+
+        return aVertices;
+    }
+
+
+    public static bool HasValidMeshData(GameObject go) {
+        return go.GetComponent<MeshFilter>() != null || go.GetComponent<SkinnedMeshRenderer>() != null;
+    }
+}
