e3ce0deb604eafdecdcaae0e66d9512392445d2b
billwillman <billwillman@qq.com>
2019-05-14 08:33:33 +0800

    11

--- Assets/LuaFrameWork/DownloadCheck.cs
+++ Assets/LuaFrameWork/DownloadCheck.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using AutoUpdate;
+using NsHttpClient;
 
 public class DownloadCheck : MonoBehaviour {
 
@@ -57,19 +58,57 @@ public class DownloadCheck : MonoBehaviour {
 		AutoUpdateMgr.Instance.Clear();
 	}
 
+	float process = 0f;
+	HttpClient m_Client = null;
+	float read, maxRead;
+
+	void OnDownEnd(HttpClient client, HttpListenerStatus status)
+	{
+		
+		switch (status) {
+		case HttpListenerStatus.hsDone:
+			{
+				process = 1f;
+				Debug.Log ("下载完成");
+				break;
+			}
+		case HttpListenerStatus.hsError:
+			{
+				Debug.Log ("下载失败");
+				break;
+			}
+		}
+		m_Client = null;
+	}
+
+	void OnDownProcess(HttpClient client)
+	{
+		var rep = client.Listener as HttpClientResponse;
+		process = rep.DownProcess;
+		read = rep.ReadBytes;
+		maxRead = rep.MaxReadBytes;
+	}
+
 	void OnGUI()
 	{
 		if (GUI.Button(new Rect(100, 100, 100, 50), "检查更新"))
 		{
-			AutoUpdateMgr.Instance.StartAutoUpdate(m_downloadUrl, 10, 64 * 1024);
-			AutoUpdateMgr.Instance.OnError = OnDownError;
-			AutoUpdateMgr.Instance.OnStateChanged = OnDownStateChanged;
+			//AutoUpdateMgr.Instance.StartAutoUpdate(m_downloadUrl, 10, 64 * 1024);
+			//AutoUpdateMgr.Instance.OnError = OnDownError;
+			//AutoUpdateMgr.Instance.OnStateChanged = OnDownStateChanged;
+			var stream = new HttpClientFileStream(Utils.FilePathMgr.GetInstance().WritePath + "/Android-1.5.0.0.zip", 0, 64 * 1024);
+			if (m_Client != null) {
+				m_Client.Dispose();
+				m_Client = null;
+			}
+			m_Client = HttpHelper.OpenUrl ("https://patch.cgm.beanfun.com/cgm/update/Android/Android-1.5.0.0.zip", stream, OnDownEnd, OnDownProcess, 30, 30);
 		}
 
-		float process = AutoUpdateMgr.Instance.DownProcess * 100f;
+		//float process = AutoUpdateMgr.Instance.DownProcess * 100f;
 		GUI.Label(new Rect(100, 150, 100, 20), string.Format("下载进度: {0:D}", (int)process));
 
-		string downStr = string.Format("{0}/{1}", AutoUpdateMgr.Instance.CurDownM, AutoUpdateMgr.Instance.CurDownM, AutoUpdateMgr.Instance.TotalDownM);
+		//string downStr = string.Format("{0}/{1}", AutoUpdateMgr.Instance.CurDownM, AutoUpdateMgr.Instance.CurDownM, AutoUpdateMgr.Instance.TotalDownM);
+		string downStr = string.Format("{0}/{1}", read, maxRead);
 		GUI.Label(new Rect(100, 170, 100, 20), downStr);
 	}
 	
