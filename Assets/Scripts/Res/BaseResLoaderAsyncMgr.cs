587731df63f2960858df7f6e86886c15ba0996b6
billwillman <billwillman@qq.com>
2020-03-10 20:43:13 +0800

    1

---
+++ Assets/Scripts/Res/BaseResLoaderAsyncMgr.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+namespace NsLib.ResMgr {
+
+     public interface IBaseResLoaderAsyncListener {
+        int UUID {
+            get;
+        }
+    }
+
+    
+
+    public class BaseResLoaderAsyncMgr : SingetonMono<BaseResLoaderAsyncMgr> {
+        private Dictionary<int, IBaseResLoaderAsyncListener> m_ListernMap = new Dictionary<int, IBaseResLoaderAsyncListener>();
+
+        public bool RegListener(IBaseResLoaderAsyncListener listener) {
+            if (listener == null)
+                return false;
+            m_ListernMap[listener.UUID] = listener;
+            return true;
+        }
+
+        public void RemoveListener(IBaseResLoaderAsyncListener listener) {
+            if (listener == null)
+                return;
+            var uuid = listener.UUID;
+            if (m_ListernMap.ContainsKey(uuid))
+                m_ListernMap.Remove(uuid);
+        }
+
+        public bool LoadTextureAsync(string fileName, IBaseResLoaderAsyncListener listener, int subID) {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Res/BaseResLoaderAsyncMono.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NsLib.ResMgr {
+
+    public class BaseResLoaderAsyncMono : MonoBehaviour, IBaseResLoaderAsyncListener {
+        private bool m_IsAppQuit = false;
+        private static int m_GlobalUUID = 0;
+        private int m_UUID = 0;
+
+        void Awake() {
+            m_UUID = ++m_GlobalUUID;
+        }
+
+        public int UUID {
+            get {
+                return m_UUID;
+            }
+        }
+
+        void OnDestroy() {
+            if (m_IsAppQuit)
+                return;
+            var mgr = BaseResLoaderAsyncMgr.GetInstance();
+            if (mgr == null)
+                return;
+            mgr.RemoveListener(this);
+        }
+
+        void OnApplicationQuit() {
+            m_IsAppQuit = true;
+        }
+    }
+
+}
